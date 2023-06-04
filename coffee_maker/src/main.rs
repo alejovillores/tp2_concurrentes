eabcf5d216c3c6ac4816b7051608173d7c6262e0
solfonte <msfontenla@fi.uba.ar>
2023-06-04 15:05:22 -0300

    feat: add order parsing to coffee maker actor

--- coffee_maker/src/main.rs
+++ coffee_maker/src/main.rs
@@ -2,7 +2,7 @@ use actix::Actor;
 use coffee_maker::{
     coffee_maker::CoffeeMaker,
     messages::{points_consuming_order::PointsConsumingOrder, take_order::TakeOrder},
-    utils::probablity_calculator::ProbabilityCalculator,
+    utils::{order_parser::OrderParser, probablity_calculator::ProbabilityCalculator},
 };
 
 const PROBABLITY: f64 = 0.8;
@@ -11,19 +11,25 @@ const PROBABLITY: f64 = 0.8;
 async fn main() {
     //let coffee_maker_arbitrer = SyncArbiter::start(2, || CoffeeMaker {});
     let probablity_calculator = ProbabilityCalculator::new();
-    match CoffeeMaker::new(PROBABLITY, probablity_calculator) {
+    let order_parser = OrderParser::new(String::from("coffee_maker/files/test_files/one_order.json"));
+    match CoffeeMaker::new(PROBABLITY, probablity_calculator, order_parser) {
         Ok(coffee_maker_actor) => {
             let addr = coffee_maker_actor.start();
 
             let read_order_result = addr.send(TakeOrder {}).await.unwrap();
-            let order_points = if read_order_result.is_ok() {
-                read_order_result.unwrap().coffee_points
+            let order_points = if read_order_result.is_some() {
+                // ver si usar option o result
+                let res = read_order_result.unwrap().coffee_points;
+                println!("entra aca {}", res);
+                res
             } else {
                 0
             };
 
             let res = addr
-                .send(PointsConsumingOrder { coffe_points: order_points })
+                .send(PointsConsumingOrder {
+                    coffe_points: order_points,
+                })
                 .await
                 .unwrap();
             print!("{}", res)
