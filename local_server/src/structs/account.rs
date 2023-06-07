2fa2cc76d89184d824c6c0c2b50ab6e5303b90b1
Juan Bautista Rodriguez <jbrodriguez@fi.uba.ar>
2023-06-07 20:38:10 -0300

    Account struct unit tests

--- local_server/src/structs/account.rs
+++ local_server/src/structs/account.rs
@@ -46,3 +46,81 @@ impl Account {
         }
     }
 }
+
+#[cfg(test)]
+mod account_test{
+    use super::*;
+
+    #[test]
+    fn test_add_points() {
+        let mut account = Account::new(123).unwrap();
+        account.add_points(15);
+        assert_eq!(account.points, 15);
+        account.add_points(10);
+        assert_eq!(account.points, 25);
+    }
+
+    #[test]
+    fn test_subtract_points_with_enough_blocked_points_success() {
+        let mut account = Account::new(123).unwrap();
+        account.points = 30;
+        account.blocked_points = 15;
+        let result = account.subtract_points(10);
+        assert_eq!(account.points, 20);
+        assert_eq!(account.blocked_points, 5);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_subtract_points_with_not_enough_blocked_points_fails(){
+        let mut account = Account::new(123).unwrap();
+        account.points = 30;
+        account.blocked_points = 5;
+        let result = account.subtract_points(10);
+        assert_eq!(account.points, 30);
+        assert_eq!(account.blocked_points, 5);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_block_points_with_enough_points_success() {
+        let mut account = Account::new(123).unwrap();
+        account.points = 15;
+        let result = account.block_points(10);
+        assert_eq!(account.points, 15);
+        assert_eq!(account.blocked_points, 10);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_block_points_with_not_enough_points_fails() {
+        let mut account = Account::new(123).unwrap();
+        account.points = 5;
+        let result = account.block_points(10);
+        assert_eq!(account.points, 5);
+        assert_eq!(account.blocked_points, 0);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_unblock_points_with_enough_blocked_points_success() {
+        let mut account = Account::new(123).unwrap();
+        account.points = 15;
+        account.blocked_points = 10;
+        let result = account.unblock_points(10);
+        assert_eq!(account.points, 15);
+        assert_eq!(account.blocked_points, 0);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_unblock_points_with_not_enough_blocked_points_fails() {
+        let mut account = Account::new(123).unwrap();
+        account.points = 15;
+        account.blocked_points = 5;
+        let result = account.unblock_points(10);
+        assert_eq!(account.points, 15);
+        assert_eq!(account.blocked_points, 5);
+        assert!(result.is_err());
+    }
+}
