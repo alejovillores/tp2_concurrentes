10f5abb36be09cadc8e6c3b1e812726a6cb1c9ed
alejovillores <avillores@fi.uba.ar>
2023-06-11 17:54:26 -0300

    fix server

--- local_server/src/main.rs
+++ local_server/src/main.rs
@@ -1,10 +1,6 @@
-extern crate actix;
-
-use actix::{Actor, Addr, MailboxError, System};
+use actix::{Actor, Addr, MailboxError};
 use local_server::structs::token::Token;
 use log::{error, info};
-use std::io::{BufRead, BufReader, Write};
-use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Condvar, Mutex};
 
 use local_server::structs::messages::UnblockPoints;
@@ -12,47 +8,52 @@ use local_server::{
     local_server::LocalServer,
     structs::messages::{AddPoints, BlockPoints, SubtractPoints},
 };
+use tokio::io::{self, split, BufReader, AsyncBufReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener,TcpStream};
 
 #[actix_rt::main]
 async fn main() {
-    let system = System::new();
+    env_logger::init();
+
     let server_address = LocalServer::new().unwrap().start();
     let token_monitor: Arc<(Mutex<Token>, Condvar)> =
         Arc::new((Mutex::new(Token::new()), Condvar::new()));
 
-    let listener = TcpListener::bind("127.0.0.1:8081").expect("Failed to bind address");
+    let listener = TcpListener::bind("127.0.0.1:8081").await.expect("Failed to bind address");
 
-    info!("Waiting for connections conexiones!");
-
-    for stream in listener.incoming() {
+    info!("Waiting for connections!");
+    loop {
         let token_monitor_clone = token_monitor.clone();
-
-        match stream {
-            Ok(stream) => {
+        info!("New TCP stream !");
+        match listener.accept().await  {
+            Ok((stream,_))=> {
                 let server_addr_clone = server_address.clone();
-                actix_rt::spawn(async move {
-                    handle_client(stream, server_addr_clone, token_monitor_clone).await;
+                tokio::spawn(async move {
+                    info!("Handling stream !");
+                    handle_client(stream, server_addr_clone, token_monitor_clone).await
                 });
             }
             Err(e) => {
                 error!("Error accepting connection: {}", e);
             }
         }
+        
     }
-
-    system.run().unwrap();
 }
 
 async fn handle_client(
-    mut stream: TcpStream,
+    stream: TcpStream,
     server_address: Addr<LocalServer>,
     token_monitor: Arc<(Mutex<Token>, Condvar)>,
 ) {
-    let reader = BufReader::new(stream.try_clone().expect(""));
-    for line in reader.lines() {
-        let token_monitor_clone = token_monitor.clone();
-        match line {
-            Ok(line) => {
+    let (r, mut w): (io::ReadHalf<TcpStream>, io::WriteHalf<TcpStream>) = split(stream);
+
+    let mut reader = BufReader::new(r);
+    loop {
+        let mut line = String::new();
+        match reader.read_line(&mut line).await {
+            Ok(_) => {
+                let token_monitor_clone = token_monitor.clone();
                 let parts: Vec<&str> = line.split(',').map(|s| s.trim()).collect();
                 if parts.len() == 3 {
                     let method = parts[0];
@@ -70,20 +71,22 @@ async fn handle_client(
                             server_address.send(msg).await
                         }
                         _ => {
-                            stream.write_all(b"ERR: Invalid method\n").unwrap();
+                            w.write_all(b"ERR: Invalid method\n").await;
                             Err(actix::MailboxError::Closed)
                         }
                     };
-                    handle_result(&mut stream, result);
+                    handle_result(&mut w, result).await;
                 } else if parts.len() == 4 {
+                    info!("{:?}", parts);
                     let method = parts[0];
                     let operation = parts[1];
-                    let customer_id: u32 = parts[2].parse().unwrap();
+                    let customer_id: u32 = parts[2].parse().expect("No puedo parsear");
                     let points: u32 = parts[3].parse().unwrap();
-
+                    
                     let result = match method {
                         "RES" => match operation {
                             "ADD" => {
+                                info!("RES package with ADD received");
                                 let msg = AddPoints {
                                     customer_id,
                                     points,
@@ -91,6 +94,8 @@ async fn handle_client(
                                 server_address.send(msg).await
                             }
                             "SUBS" => {
+                                info!("RES package with SUBS received");
+
                                 let msg = SubtractPoints {
                                     customer_id,
                                     points,
@@ -98,6 +103,7 @@ async fn handle_client(
                                 server_address.send(msg).await
                             }
                             "UNBL" => {
+                                info!("RES package with UNBL received");
                                 let msg = UnblockPoints {
                                     customer_id,
                                     points,
@@ -105,18 +111,19 @@ async fn handle_client(
                                 server_address.send(msg).await
                             }
                             _ => {
-                                stream.write_all(b"ERR: Invalid operation\n").unwrap();
+                                error!("RES package with no valid operation received");
+                                w.write_all(b"ERR: Invalid operation\n").await.expect("error");
                                 Err(actix::MailboxError::Closed)
                             }
                         },
                         _ => {
-                            stream.write_all(b"ERR: Invalid method\n").unwrap();
+                            w.write_all(b"ERR: Invalid method\n").await.expect("error");
                             Err(actix::MailboxError::Closed)
                         }
                     };
-                    handle_result(&mut stream, result);
+                    handle_result(&mut w, result);
                 } else {
-                    stream.write_all(b"ERR: Invalid format\n").unwrap();
+                    w.write_all(b"ERR: Invalid format\n").await.expect("error");
                 }
             }
             Err(e) => {
@@ -127,13 +134,13 @@ async fn handle_client(
     }
 }
 
-fn handle_result(stream: &mut TcpStream, result: Result<String, MailboxError>) {
+async fn handle_result(w: &mut io::WriteHalf<TcpStream>, result: Result<String, MailboxError>) {
     match result {
         Ok(res) => {
-            stream.write_all(format!("{}\n", res).as_bytes()).unwrap();
+            w.write_all(format!("{}\n", res).as_bytes()).await.expect("error");
         }
         Err(_) => {
-            stream.write_all(b"ERR: Internal server error\n").unwrap();
+            w.write_all(b"ERR: Internal server error\n").await.expect("error");
         }
     }
 }
