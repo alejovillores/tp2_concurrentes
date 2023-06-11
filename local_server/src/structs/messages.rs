b3d8fe20854ce0dae2330d4e0cf7eff14e642edf
Juan Bautista Rodriguez <jbrodriguez@fi.uba.ar>
2023-06-11 15:07:23 -0300

    Unblock messafe struct

--- local_server/src/structs/messages.rs
+++ local_server/src/structs/messages.rs
@@ -23,6 +23,13 @@ pub struct SubtractPoints {
 
 #[derive(Message, Debug)]
 #[rtype(result = "String")]
+pub struct UnblockPoints {
+    pub customer_id: u32,
+    pub points: u32,
+}
+
+#[derive(Message, Debug)]
+#[rtype(result = "String")]
 pub struct SyncAccount {
     pub customer_id: u32,
     pub points: u32,
